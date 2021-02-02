4f9b72bfa51c18d9b6a192db690db8855b00d5e9
傅培禎 <a0985562570@gmail.com>
2021-02-02 17:54:39 +0800

    Initial Commit

---
+++ DigimoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigimoApp
+//
+//  Created by 傅培禎 on 2021/2/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
